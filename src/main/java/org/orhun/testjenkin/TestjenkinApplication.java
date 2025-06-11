3030cde6ace9debd3bf547d7ad12f29710250388
okuorhun <oku.orhun@gmail.com>
2025-06-11 12:56:19 -0400

    test this project with jenkins

---
+++ src/main/java/org/orhun/testjenkin/TestjenkinApplication.java
@@ -0,0 +1,13 @@
+package org.orhun.testjenkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestjenkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestjenkinApplication.class, args);
+    }
+
+}
