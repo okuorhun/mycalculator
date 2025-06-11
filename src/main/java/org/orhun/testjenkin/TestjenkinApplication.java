f7ce8a529df6e2cb050b9cc07a5b2487b88092f1
okuorhun <oku.orhun@gmail.com>
2025-06-11 13:38:04 -0400

    test file added

--- src/main/java/org/orhun/testjenkin/TestjenkinApplication.java
+++ src/main/java/org/orhun/testjenkin/TestjenkinApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestjenkinApplication {
 
     public static void main(String[] args) {
+
+
         SpringApplication.run(TestjenkinApplication.class, args);
+        System.out.println(" Murat Orhun This is a test...");
     }
 
 }
