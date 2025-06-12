3dff51c1c3acdd6f21de5793014e5f52cc125a76
okuorhun <oku.orhun@gmail.com>
2025-06-12 10:25:15 -0400

    test case added

--- src/test/java/org/orhun/testjenkin/TestjenkinApplicationTests.java
+++ src/test/java/org/orhun/testjenkin/TestjenkinApplicationTests.java
@@ -3,11 +3,14 @@ package org.orhun.testjenkin;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class TestjenkinApplicationTests {
 
     @Test
     void contextLoads() {
+        assertTrue(true);
     }
 
 }
